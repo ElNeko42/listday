d14fe0f8d4547aeb3cb221a305c0a0501c41c128
elneko42 <franson177@gmail.com>
2024-02-21 20:04:20 +0100

    link component

---
+++ resources/js/MyComponents/Link.vue
@@ -0,0 +1,38 @@
+<template>
+    <a
+      :href="url"
+      :class="`text-gray-700 underline ${linkClass}`"
+      @click.prevent="handleClick"
+    >
+      {{ text }}
+    </a>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Link',
+    props: {
+      text: {
+        type: String,
+        default: 'Click here',
+      },
+      url: {
+        type: String,
+        default: '#',
+      },
+      linkClass: {
+        type: String,
+        default: '',
+      },
+    },
+    methods: {
+      handleClick() {
+        this.$emit('click');
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  

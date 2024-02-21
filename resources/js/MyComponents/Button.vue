9b1a77cac32340249027ed86e5c0a988cc9e2430
elneko42 <franson177@gmail.com>
2024-02-21 20:03:04 +0100

    Refactor login form component

---
+++ resources/js/MyComponents/Button.vue
@@ -0,0 +1,39 @@
+<template>
+    <button
+      :class="`inline-block shrink-0 rounded-md px-12 py-3 text-sm font-medium transition focus:outline-none ${buttonClass}`"
+      :type="buttonType"
+      @click="onClick"
+    >
+      {{ buttonText }}
+    </button>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Button',
+    props: {
+      buttonText: {
+        type: String,
+        default: 'Button',
+      },
+      buttonType: {
+        type: String,
+        default: 'button', 
+      },
+      buttonClass: {
+        type: String,
+        default: 'border border-blue-600 bg-blue-600 text-white hover:bg-transparent hover:text-blue-600 focus:ring active:text-blue-500',
+      },
+    },
+    methods: {
+      onClick() {
+        this.$emit('click');
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+
+  </style>
+  
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -28,68 +28,19 @@
                             <InputLabel label="Email" v-model:inputValue="userEmail" type="email" inputId="user-email" />
                         </div>
 
-
-                        <div class="col-span-6 sm:col-span-3">
-                            <label for="LastName" class="block text-sm font-medium text-gray-700">
-                                Last Name
-                            </label>
-
-                            <input type="text" id="LastName" name="last_name"
-                                class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm" />
-                        </div>
-
                         <div class="col-span-6">
-                            <label for="Email" class="block text-sm font-medium text-gray-700"> Email </label>
-
-                            <input type="email" id="Email" name="email"
-                                class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm" />
+                            <InputLabel label="Password" type="password" inputId="user-password" />
                         </div>
 
-                        <div class="col-span-6 sm:col-span-3">
-                            <label for="Password" class="block text-sm font-medium text-gray-700"> Password </label>
 
-                            <input type="password" id="Password" name="password"
-                                class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm" />
-                        </div>
-
-                        <div class="col-span-6 sm:col-span-3">
-                            <label for="PasswordConfirmation" class="block text-sm font-medium text-gray-700">
-                                Password Confirmation
-                            </label>
-
-                            <input type="password" id="PasswordConfirmation" name="password_confirmation"
-                                class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm" />
-                        </div>
-
-                        <div class="col-span-6">
-                            <label for="MarketingAccept" class="flex gap-4">
-                                <input type="checkbox" id="MarketingAccept" name="marketing_accept"
-                                    class="size-5 rounded-md border-gray-200 bg-white shadow-sm" />
-
-                                <span class="text-sm text-gray-700">
-                                    I want to receive emails about events, product updates and company announcements.
-                                </span>
-                            </label>
-                        </div>
-
-                        <div class="col-span-6">
-                            <p class="text-sm text-gray-500">
-                                By creating an account, you agree to our
-                                <a href="#" class="text-gray-700 underline"> terms and conditions </a>
-                                and
-                                <a href="#" class="text-gray-700 underline">privacy policy</a>.
-                            </p>
-                        </div>
 
                         <div class="col-span-6 sm:flex sm:items-center sm:gap-4">
-                            <button
-                                class="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500">
-                                Create an account
-                            </button>
+                            <Button buttonText="Iniciar sesión"
+                                buttonClass="border border-blue-600 bg-blue-600 text-white hover:bg-transparent hover:text-blue-600 focus:ring active:text-blue-500"
+                                @click="handleLogin" />
 
                             <p class="mt-4 text-sm text-gray-500 sm:mt-0">
-                                Already have an account?
-                                <a href="#" class="text-gray-700 underline">Log in</a>.
+                                <a href="#" class="text-gray-700 underline">haz olvidado la contraseña?</a>.
                             </p>
                         </div>
                     </form>
@@ -102,10 +53,11 @@
 <script>
 import { Input } from 'postcss';
 import InputLabel from '../../MyComponents/InputLabel.vue';
-
+import Button from '../../MyComponents/Button.vue';
 export default {
     components: {
         InputLabel,
+        Button,
         Input
     },
     data() {

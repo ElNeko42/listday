88b55551cd685a0edb48fcf2f6e661f4fb8aa009
elneko42 <franson177@gmail.com>
2024-02-22 13:41:22 +0100

    Refactor TaskList component and add Font Awesome icons

--- resources/js/MyComponents/TaskList.vue
+++ resources/js/MyComponents/TaskList.vue
@@ -1,53 +1,46 @@
 <template>
     <div class="space-y-4">
-        <div class="mt-5">
-            <!-- Lista de tareas -->
-            <ul class="space-y-3">
-                <li v-for="(task, index) in tasks" :key="index"
-                    class="flex items-center justify-between p-3 rounded shadow">
-                    <div class="flex items-center">
-                        <input type="checkbox" v-model="task.completed" @change="toggleTaskCompletion(task)"
-                            class="mr-2 h-4 w-4">
-                        <span :class="{ 'line-through': task.completed }">{{ task.name }}</span>
-                    </div>
-                    <div class="flex items-center">
-                        <button @click="editTask(task)" class="p-1.5 mr-2 text-blue-500 hover:bg-blue-100 rounded">
-                            <PencilIcon class="h-5 w-5" />
-                        </button>
-                        <button @click="deleteTask(task)" class="p-1.5 text-red-500 hover:bg-red-100 rounded">
-                            <TrashIcon class="h-5 w-5" />
-                        </button>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </div>
-</template>
-  
-<script>
-import { PencilIcon, TrashIcon } from '@heroicons/vue/solid';
+      <div class="mt-5">
+        <!-- Lista de tareas -->
+        <ul class="space-y-3">
+          <li v-for="(task, index) in tasks" :key="index" class="flex items-center justify-between p-3 rounded shadow">
+            <div class="flex items-center">
+              <input type="checkbox" v-model="task.completed" @change="toggleTaskCompletion(task)" class="mr-2 h-4 w-4">
+              <span :class="{'line-through': task.completed}">{{ task.name }}</span>
+            </div>
+            <div class="flex items-center">
+              <button @click="editTask(task)" class="p-1.5 mr-2 text-blue-500 hover:bg-blue-100 rounded">
+                <i class="fas fa-pencil-alt"></i> 
 
-export default {
+              </button>
+              <button @click="deleteTask(task)" class="p-1.5 text-red-500 hover:bg-red-100 rounded">
+                <i class="fas fa-trash"></i>
+            </button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
     name: 'TaskList',
-    components: {
-        PencilIcon,
-        TrashIcon
-    },
     props: {
-        tasks: Array
+      tasks: Array
     },
     methods: {
-        toggleTaskCompletion(task) {
-            task.completed = !task.completed;
-
-        },
-        editTask(task) {
-
-        },
-        deleteTask(task) {
-
-        }
+      toggleTaskCompletion(task) {
+        task.completed = !task.completed;
+       
+      },
+      editTask(task) {
+        
+      },
+      deleteTask(task) {
+        
+      }
     }
-}
-</script>
+  }
+  </script>
   
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 
         <!-- Styles -->
         <style>
